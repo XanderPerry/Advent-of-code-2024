30ff5829bb25e97cdad2ab710543b2a9cc5f0737
Xander <Xanderperry@hotmail.nl>
2024-12-01 22:22:26 +0100

    Completed part 2 of day 1

--- Advent-of-code-2024/Day_1.cpp
+++ Advent-of-code-2024/Day_1.cpp
@@ -3,13 +3,20 @@
 #include <string>
 #include <algorithm>
 
-#define DEBUG false
+#define DEBUG_P1 false
+#define DEBUG_P2 false
 
 #define LINE_LEN 13
 #define N_LINES 1000
 
 int main()
 {
+	///////////////////////////////////////////////
+	//
+	//			PART 1
+	//
+	//////////////////////////////////////////////
+
 	//	Open data file
 	std::ifstream file("Day_1_list.txt");
 	if (!file.is_open())
@@ -25,26 +32,27 @@ int main()
 	std::string CurrentLine;
 	for(int i = 0; std::getline(file, CurrentLine); i++)
 	{
-		if (DEBUG) { std::cout << "[" << i << "]	" << CurrentLine << std::endl; }
+		if (DEBUG_P1) { std::cout << "[" << i << "]	" << CurrentLine << std::endl; }
 
 		//	Extract numbers as string
 		std::string num1 = CurrentLine.substr(0, 5);
 		std::string num2 = CurrentLine.substr(8, 11);
-		if (DEBUG) { std::cout << "num1: " << num1 << "	num2: " << num2 << std::endl; }
+		if (DEBUG_P1) { std::cout << "num1: " << num1 << "	num2: " << num2 << std::endl; }
 
 		//	Convert numbers to int and add to list
 		List1[i] = stoi(num1);
 		List2[i] = stoi(num2);
-		if (DEBUG) { std::cout << "List1: " << List1[i] << "	List2: " << List2[i] << std::endl; }
+		if (DEBUG_P1) { std::cout << "List1: " << List1[i] << "	List2: " << List2[i] << std::endl; }
 
-		if (DEBUG) { std::cout << std::endl; }
+		if (DEBUG_P1) { std::cout << std::endl; }
 	}
+	file.close();
 	
 	//	Sort lists
 	std::sort(std::begin(List1), std::end(List1));
 	std::sort(std::begin(List2), std::end(List2));
 
-	if (DEBUG)
+	if (DEBUG_P1)
 	{
 		std::cout << "First elements of sorted lists:" << std::endl;
 		for (int i = 0; i < 10; i++)
@@ -56,7 +64,7 @@ int main()
 	for (int i = 0; i < N_LINES; i++)
 	{
 		TotalDist += abs(List1[i] - List2[i]);
-		if (DEBUG)
+		if (DEBUG_P1)
 		{
 			std::cout << i << "	|" << List1[i] << " - " << List2[i] << "| = " << abs(List1[i] - List2[i]) << std::endl;
 			std::cout << "Current distance: " << TotalDist << std::endl;
@@ -65,7 +73,36 @@ int main()
 	
 	std::cout << "The total distance is: " << TotalDist << std::endl;
 
-	file.close();
+
+	///////////////////////////////////////////////
+	//
+	//			PART 2
+	//
+	//////////////////////////////////////////////
+
+	int similarity = 0;
+	for (int i = 0; i < N_LINES; i++)
+	{
+		//	Count appearences of current item in List2
+		int n_appearances = 0;
+		for (int j = 0; j < N_LINES; j++)
+			if (List1[i] == List2[j])
+				n_appearances++;
+
+		//	Calculate similarity
+		similarity += List1[i] * n_appearances;
+
+		if (DEBUG_P2) 
+		{ 
+			std::cout << List1[i] << " appears " << n_appearances << " times" << std::endl; 
+			std::cout << List1[i] << " * " << n_appearances << " = " << List1[i] * n_appearances << std::endl;
+			std::cout << "Current similarity is: " << similarity << std::endl;
+			std::cout << std::endl;
+		}
+	}
+
+	std::cout << "List similarity is: " << similarity << std::endl;
+
 	return 0;
 }
 
